821eda88f0976558c3aa69ca9c27052388b3fa59
VIGNESHDASK <41521907+VIGNESHDASK@users.noreply.github.com>
2021-01-28 00:08:29 +0530

    Update settings.py

--- kochikuri/settings.py
+++ kochikuri/settings.py
@@ -129,10 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
-]
 #STATIC_ROOT = os.path.join(BASE_DIR,'assets')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())

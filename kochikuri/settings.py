560177c06816ceee470130bef1e13573065a449e
VIGNESHDASK <vigneshdas4444@gmail.com>
2021-01-27 23:39:46 +0530

    first commit

--- kochikuri/settings.py
+++ kochikuri/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zzkz+34vw0oph#tau)dz6kqpaj=afp0xuzrz*j^rvmoq%6@fh2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
